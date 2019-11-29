621f049815e2a1a7d63543e4272ee2fc0ddd0b63
aktamaa <aktama8998@gmail.com>
2019-11-29 11:30:15 +0700

    update lagi

--- application/models/Model_Tiket.php
+++ application/models/Model_Tiket.php
@@ -54,11 +54,63 @@ class Model_Tiket extends CI_Model
         $this->id_tiket = uniqid();
         $this->id_acara = $post["id_acara"];
         $this->id_customer = $post["id_customer"];
+
+        $this->load->library('ciqrcode'); //pemanggilan library QR CODE
+ 
+        $config['cacheable']    = true; //boolean, the default is true
+        $config['cachedir']     = './upload/tiket'; //string, the default is application/cache/
+        $config['errorlog']     = './upload/tiket'; //string, the default is application/logs/
+        $config['imagedir']     = './upload/tiket/qr_code/'; //direktori penyimpanan qr code
+        $config['quality']      = true; //boolean, the default is true
+        $config['size']         = '1024'; //interger, the default is 1024
+        $config['black']        = array(224,255,255); // array, default is array(255,255,255)
+        $config['white']        = array(70,130,180); // array, default is array(0,0,0)
+        $this->ciqrcode->initialize($config);
+ 
+        $image_name=$id_tiket.'.png'; //buat name dari qr code sesuai dengan nim
+ 
+        $params['data'] = $id_tiket; //data yang akan di jadikan QR CODE
+        $params['level'] = 'H'; //H=High
+        $params['size'] = 10;
+        $params['savename'] = FCPATH.$config['imagedir'].$image_name; //simpan image QR CODE ke folder assets/images/
+        $this->ciqrcode->generate($params);
+
         $this->qr_code = $post["qr_code"];
         $this->jumlah = $post["jumlah"];
         $this->total_harga = $post["harga"]*$post["jumlah"];
         $this->status = $post["status"];
         $this->bukti_bayar = 'default.jpg';
         $this->db->insert($this->_table, $this);
+    }
+
+    public function generate()
+    {
+
+            $data = array ('success' => false, 'messages'=> array());       
+            $valid = $this->form_validation;
+
+            $valid->set_rules('code','Code','trim|required|xss_clean');                                  
+            $valid->set_error_delimiters('<p class="text-danger">', '</p>');
+            if($valid->run() == FALSE)
+            {
+                    foreach($_POST as $key => $value) 
+                    {
+                            $data['messages'][$key] = form_error($key);
+                    }               
+            
+            }
+            else
+            {
+                    $code=$this->input->post('code');              
+                    $img = $code.'.png';
+                    $qrcode = $this->_code($code);
+                // $this->siswa_model->simpan_siswa($nim,$nama,$jurusan,$code); //simpan ke database
+        $data['info'] = "<img src=".base_url('upload/tiket/qr_code/'.$img)." class='rounded mx-auto d-block'>";
+                    $data['success'] = true;   
+                    //$data['redirect'] = base_url();     
+                    
+            }
+            echo json_encode($data);        
+
     }
 }
